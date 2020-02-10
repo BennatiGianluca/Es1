5be6ec98731dc067ab8dfaadd693688be2e3dcad
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-02-10 09:52:38 +0100

    f

--- Es1/Program.cs
+++ Es1/Program.cs
@@ -16,7 +16,7 @@ namespace Es1
             Console.WriteLine("Quanti numeri vuoi inserire?");
             int n = int.Parse(Console.ReadLine());
             List<int> lista = new List<int>(n);
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i< n; i++)
             {
                 Console.WriteLine("Inserisci un numero: ");
                 int numero = int.Parse(Console.ReadLine());
